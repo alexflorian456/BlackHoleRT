53835dc341ca54e2448459d1b8588a15c1926a70
alexflorian456 <alex.florian@stud.ubbcluj.ro>
2024-03-24 18:38:43 +0200

    Added glfw for on-screen rendering

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,4 @@
 #include <cstdio>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cuda_runtime_api.h>
-#include <cuda_texture_types.h>
-#include <device_launch_parameters.h>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -11,15 +6,22 @@
 #include <climits>
 #include <chrono>
 #include <ctime>
-#include <numbers>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <cuda_texture_types.h>
+#include <cuda_fp16.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
-#define PI 3.14159265358979323846
-#define GRAVITATIONAL_CONSTANT 6.674e-3 // real gravitational constant is 6.674e-11
+#include <GLFW/glfw3.h>
+
+#define PI 3.14159265358979323846f
+#define GRAVITATIONAL_CONSTANT 6.674e-3f // real gravitational constant is 6.674e-11
 #define RAY_TIME_RESOLUTION 1 // time between two ray positions when computing gravitational lensing
 #define RAY_MAX_ITERATIONS 100 // maximum amount of ray positions computed per pixel
 
@@ -30,44 +32,55 @@ void cudaHandleError(cudaError_t cudaResult){
     }
 }
 
-__device__ __host__ double
-radians_to_degrees(double radians){
-    return radians * 180.0 / PI;
+__device__ __host__ float
+radians_to_degrees(float radians){
+    return radians * 180.0f / PI;
 }
 
-__device__ __host__ double
-degrees_to_radians(double degrees){
-    return degrees * PI / 180.0;
+__device__ __host__ float
+degrees_to_radians(float degrees){
+    return degrees * PI / 180.0f;
+}
+
+// Function to initialize OpenGL
+void initOpenGL(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, 0, height, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glEnable(GL_TEXTURE_2D);
 }
 
 class 
 Vector{ // TODO: change to float or half? - maybe template
 
     public:
-        double x;
-        double y;
-        double z;
+        float x;
+        float y;
+        float z;
 
         __device__ __host__
-        Vector(double x, double y, double z):
+        Vector(float x, float y, float z):
             x(x),
             y(y),
             z(z)
         {}
 
-        __device__ __host__ double
+        __device__ __host__ float
         length_2() const{
             return x * x + y * y + z * z;
         }
 
-        __device__ __host__ double
+        __device__ __host__ float
         length() const{
             return std::sqrt(length_2());
         }
 
         __device__ __host__ Vector
         normalize() const{
-            double norm = length();
+            float norm = length();
             if(norm > 0){
                 return Vector(x / norm, y / norm, z / norm);
             }
@@ -84,7 +97,7 @@ Vector{ // TODO: change to float or half? - maybe template
             return Vector(x - b.x, y - b.y, z - b.z);
         }
 
-        __device__ __host__ double
+        __device__ __host__ float
         operator*(const Vector& b) const{
             return x * b.x + y * b.y + z * b.z;
         }
@@ -95,12 +108,12 @@ Vector{ // TODO: change to float or half? - maybe template
         }
 
         __device__ __host__ Vector
-        operator*(double k) const{
+        operator*(float k) const{
             return Vector(x * k, y * k, z * k);
         }
 
         __device__ __host__ Vector
-        operator/(double k) const{
+        operator/(float k) const{
             return Vector(x / k, y / k, z / k);
         }
 
@@ -192,16 +205,16 @@ Camera{
         Vector direction;
         Vector up;
 
-        double view_plane_distance;
-        double view_plane_width;
-        double view_plane_height;
+        float view_plane_distance;
+        float view_plane_width;
+        float view_plane_height;
 
-        double front_plane_distance;
-        double back_plane_distance;
+        float front_plane_distance;
+        float back_plane_distance;
 
         Camera( Vector position, Vector direction, Vector up,
-                double view_plane_distance, double view_plane_width, double view_plane_height,
-                double front_plane_distance, double back_plane_distance):
+                float view_plane_distance, float view_plane_width, float view_plane_height,
+                float front_plane_distance, float back_plane_distance):
 
             position(position), direction(direction), up(up),
             view_plane_distance(view_plane_distance), view_plane_width(view_plane_width), view_plane_height(view_plane_height),
@@ -210,15 +223,15 @@ Camera{
 
         Camera( int output_width, int output_height, int field_of_view,
                 Vector position, Vector direction, Vector up,
-                double front_plane_distance, double back_plane_distance):
+                float front_plane_distance, float back_plane_distance):
             
             position(position), direction(direction), up(up),
             front_plane_distance(front_plane_distance), back_plane_distance(back_plane_distance)
         {
             view_plane_distance = 1;
-            int theta = 90 - field_of_view / 2;
-            view_plane_width = 2 * view_plane_distance / std::tan(degrees_to_radians(theta));
-            double aspect_ratio = (double)output_width / (double)output_height;
+            float theta = 90.f - float(field_of_view) / 2.f;
+            view_plane_width = 2.f * view_plane_distance / std::tan(degrees_to_radians(theta));
+            float aspect_ratio = (float)output_width / (float)output_height;
             view_plane_height = view_plane_width / aspect_ratio;
         }
 };
@@ -227,10 +240,10 @@ class
 BlackHole {
 
     public:
-        double mass;
+        float mass;
         Vector position;
 
-        BlackHole(double mass, Vector position):
+        BlackHole(float mass, Vector position):
             mass(mass),
             position(position)
         {}
@@ -243,26 +256,26 @@ set_image_pixel(unsigned char * pixels, int i, int j, int output_width, Color co
     pixels[i * output_width * 3 + j * 3 + 2] = color.blue;
 }
 
-__device__ double
-image_to_view_plane(int n, int img_size, double view_plane_size){
+__device__ float
+image_to_view_plane(int n, int img_size, float view_plane_size){
     return - n * view_plane_size / img_size + view_plane_size / 2;
 }
 
-__device__ __host__ double
+__device__ __host__ float
 angle_between_vectors(Vector a, Vector b){ // result between 0 and 180
-    double dot_product = a * b;
-    double length_a = a.length();
-    double length_b = b.length();
+    float dot_product = a * b;
+    float length_a = a.length();
+    float length_b = b.length();
 
     return radians_to_degrees(std::acos(dot_product / (length_a * length_b)));
 }
 
 __device__ Color
-extract_texture_color(cudaTextureObject_t texture_object, double i, double j, int skybox_width){
+extract_texture_color(cudaTextureObject_t texture_object, float i, float j, int skybox_width){
     return Color(
-        tex2D<float4>(texture_object, j, i).x,
-        tex2D<float4>(texture_object, j, i).y,
         tex2D<float4>(texture_object, j, i).z,
+        tex2D<float4>(texture_object, j, i).y,
+        tex2D<float4>(texture_object, j, i).x,
         0
     );
 }
@@ -285,8 +298,8 @@ BlackHole * black_holes, int num_black_holes
     // initial ray direction
     Vector view_parallel = (camera.up ^ camera.direction).normalize();
     Vector camera_to_view_plane = camera.direction * camera.view_plane_distance;
-    double image_to_view_plane_width    = image_to_view_plane(j, output_width , camera.view_plane_width);
-    double image_to_view_plane_height   = image_to_view_plane(i, output_height, camera.view_plane_height);
+    float image_to_view_plane_width    = image_to_view_plane(j, output_width , camera.view_plane_width);
+    float image_to_view_plane_height   = image_to_view_plane(i, output_height, camera.view_plane_height);
     Vector ray_direction = camera_to_view_plane + view_parallel * image_to_view_plane_width + camera.up * image_to_view_plane_height;
 
     // gravitational lensing computation
@@ -294,8 +307,8 @@ BlackHole * black_holes, int num_black_holes
     Vector old_velocity = ray_direction; // TODO?: try normalize
     Vector new_position = Vector::Zero();
     Vector new_velocity = Vector::Zero();
-    double gravitational_constant = GRAVITATIONAL_CONSTANT;
-    double ray_time_resolution = RAY_TIME_RESOLUTION;
+    float gravitational_constant = GRAVITATIONAL_CONSTANT;
+    float ray_time_resolution = RAY_TIME_RESOLUTION;
                                                 /* to paint a pixel black, the ray has to be stuck */
     int escape_sphere_radius = 5;               /* in a sphere of radius = escape_sphere_radius    */
     Vector escape_sphere_center = old_position; /* centered in escape_sphere_center                */
@@ -304,8 +317,8 @@ BlackHole * black_holes, int num_black_holes
         Vector resultant_force = Vector::Zero();
         for(int black_hole_index=0; black_hole_index<num_black_holes; black_hole_index++){ // TO STUDY?: extract to separate kernel
             Vector black_hole_position = black_holes[black_hole_index].position;
-            double black_hole_mass = black_holes[black_hole_index].mass;
-            double r_squared = (black_hole_position - old_position).length_2();
+            float black_hole_mass = black_holes[black_hole_index].mass;
+            float r_squared = (black_hole_position - old_position).length_2();
             Vector r_hat = (black_hole_position - old_position).normalize();
             resultant_force += r_hat * gravitational_constant * black_hole_mass / r_squared;
         }
@@ -332,18 +345,18 @@ BlackHole * black_holes, int num_black_holes
     ray_direction = new_velocity;
     
     // skybox rendering
-    double elevation_angle = angle_between_vectors(Vector::Up(), ray_direction); // 0 degrees <=> straight up, 180 degress <=> straight down
+    float elevation_angle = angle_between_vectors(Vector::Up(), ray_direction); // 0 degrees <=> straight up, 180 degress <=> straight down
     // project ray_direction on xOz to calculate azimuth
     Vector ray_direction_projection_on_xOz = Vector(ray_direction.x, 0, ray_direction.z); // TODO: reuse ray_direction, dont declare new var
-    double projection_north_angle = angle_between_vectors(ray_direction_projection_on_xOz, Vector::North());
-    double azimuth_angle = ray_direction_projection_on_xOz.z > 0 ?
+    float projection_north_angle = angle_between_vectors(ray_direction_projection_on_xOz, Vector::North());
+    float azimuth_angle = ray_direction_projection_on_xOz.z > 0 ?
                            projection_north_angle :
                     (360 - projection_north_angle); // if z component is negative => azimuth angle > 180 degrees
 
     // TO STUDY: why did i need printf("") before?
 
-    double skybox_height_coordinate = elevation_angle / 180;
-    double skybox_width_coordinate  = azimuth_angle   / 360;
+    float skybox_height_coordinate = elevation_angle / 180;
+    float skybox_width_coordinate  = azimuth_angle   / 360;
 
     Color skybox_color = extract_texture_color(skybox_texture_object, skybox_height_coordinate, skybox_width_coordinate, skybox_width);
 
@@ -351,7 +364,7 @@ BlackHole * black_holes, int num_black_holes
 }
 
 int
-main(int argc, char ** argv){
+main(int argc, char * argv[]){
 /*
 args:
 1 - output width
@@ -365,15 +378,15 @@ args:
     Vector camera_position  = Vector::Zero();
     Vector camera_direction = Vector::South();
     Vector camera_up        = Vector::Up();
-    const double front_plane_distance   = 0;
-    const double back_plane_distance    = 1000;
-    const double camera_distance_from_center = 60;
+    const float front_plane_distance   = 0;
+    const float back_plane_distance    = 1000;
+    const float camera_distance_from_center = 60;
     Camera camera(  output_width, output_height, field_of_view,
                     camera_position, camera_direction, camera_up,
                     front_plane_distance, back_plane_distance);
 
     unsigned char * d_pixels;
-    unsigned char * h_pixels = (unsigned char *)malloc(output_width * output_height * 4 * sizeof(unsigned char));
+    unsigned char * h_pixels = (unsigned char *)malloc(output_width * output_height * 3 * sizeof(unsigned char)); // TODO: check if 3 or 4
     std::vector<unsigned char> h_skybox;
     unsigned int skybox_width   = UINT32_MAX;
     unsigned int skybox_height  = UINT32_MAX;
@@ -431,7 +444,7 @@ args:
 
     // declaring black holes
     h_black_holes.push_back(BlackHole(500, Vector(0, 0, 0)));
-    h_black_holes.push_back(BlackHole(500, Vector(0, 0, 40)));
+    // h_black_holes.push_back(BlackHole(500, Vector(0, 0, 40)));
     int num_black_holes = (int)h_black_holes.size();
 
     // allocating device memory
@@ -439,14 +452,40 @@ args:
     cudaHandleError(cudaMalloc((void **)&d_black_holes, num_black_holes * sizeof(BlackHole))); // TODO: mallocManaged?
     cudaHandleError(cudaMemcpy(d_black_holes, h_black_holes.data(), num_black_holes * sizeof(BlackHole), cudaMemcpyHostToDevice));
 
-    double angle = 0;
-    double progress_percent = -1;
+    // initialize GLFW
+    GLFWwindow* window;
+
+    if(!glfwInit()){
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if(!window){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    initOpenGL(output_width, output_height);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, output_width, output_height, 0, GL_RGB, GL_UNSIGNED_BYTE, h_pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    float angle = 0;
+    float progress_percent = -1;
     auto t_start = std::chrono::high_resolution_clock::now();
     camera.position = Vector(60, 0, 0);
 
-    double black_hole_distance = 40;
+    float black_hole_distance = 40;
 
-    while(angle < 360){ // TODO?: call multiple kernels from multiple threads
+    glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, 1920, 1080, GLFW_DONT_CARE);
+
+    while(angle < 360 && !glfwWindowShouldClose(window)){ // TODO?: call multiple kernels from multiple threads
         // SPIN IN THE CENTER:
         // camera.direction = Vector(std::cos(degrees_to_radians(angle)), 0, std::sin(degrees_to_radians(angle)));
         // ORBIT AROUND CENTER:
@@ -454,7 +493,7 @@ args:
         // camera.direction = (Vector::Zero() - camera.position).normalize();
         // MOVE INTO CENTER:
         // TODO: issue noticed when camera is at "perfect" integer coordinates, got CUDA error: an illegal memory access was encountered
-        // camera.position = camera.position + camera.direction;
+        camera.position = camera.position + camera.direction / 2;
 
         int remaining_width = output_width;
         int grid_index = 0;
@@ -471,18 +510,17 @@ args:
             else{ // remainder kernel call
                 ray<<<output_height, remaining_width>>>(camera, skybox_texture_object, d_pixels, output_width, output_height, skybox_width, skybox_height, grid_index, threads_per_block, d_black_holes, num_black_holes);                
             }
-            // TODO: decomment
-            // cudaError_t cudaError = cudaGetLastError();
-            // if (cudaError != cudaSuccess) {
-            //     fprintf(stderr, "CUDA error line 468: %s: %s\n", cudaGetErrorName(cudaError), cudaGetErrorString(cudaError));
-            //     exit(1);
-            // }
+            cudaError_t cudaError = cudaGetLastError();
+            if (cudaError != cudaSuccess) {
+                fprintf(stderr, "CUDA error line 468: %s: %s\n", cudaGetErrorName(cudaError), cudaGetErrorString(cudaError));
+                exit(1);
+            }
             remaining_width -= threads_per_block;
             grid_index++;
         }
         cudaDeviceSynchronize();
         auto frame_end = std::chrono::high_resolution_clock::now();
-        double frame_duration = std::chrono::duration<double, std::milli>(frame_end - frame_start).count();
+        float frame_duration = std::chrono::duration<float, std::milli>(frame_end - frame_start).count();
         // printf("frame%03d time: %f\n", (int)(angle*2.), frame_duration);
         auto other_start = std::chrono::high_resolution_clock::now();
         cudaError_t cudaError = cudaGetLastError();
@@ -491,17 +529,34 @@ args:
             exit(1);
         }
 
-        char * output_path = (char*)malloc(25 * sizeof(char)); //TODO: free
-        sprintf(output_path, "output\\frame%03d.png", (int)(angle*2.));
         cudaHandleError(cudaMemcpy(h_pixels, d_pixels, output_width * output_height * 3 * sizeof(unsigned char), cudaMemcpyDeviceToHost));
-        
-        bool imwrite_success = cv::imwrite(output_path, cv::Mat(cv::Size(output_width, output_height), CV_8UC3, h_pixels));
-        if(!imwrite_success){
-            fprintf(stderr, "Failed to imwrite output frame\n");
-            exit(1);
-        }
 
-        double current_progress_percent = angle / 360.0 * 100;
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, output_width, output_height, GL_RGB, GL_UNSIGNED_BYTE, h_pixels);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0); glVertex2f(0, 0);
+        glTexCoord2f(1, 0); glVertex2f((GLfloat)output_width, 0);
+        glTexCoord2f(1, 1); glVertex2f((GLfloat)output_width, (GLfloat)output_height);
+        glTexCoord2f(0, 1); glVertex2f(0, (GLfloat)output_height);
+        glEnd();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+
+        // // save to output
+        // char * output_path = (char*)malloc(25 * sizeof(char)); //TODO: free
+        // sprintf(output_path, "output\\frame%03d.png", (int)(angle*2.));
+        // bool imwrite_success = cv::imwrite(output_path, cv::Mat(cv::Size(output_width, output_height), CV_8UC3, h_pixels));
+        // if(!imwrite_success){
+        //     fprintf(stderr, "Failed to imwrite output frame\n");
+        //     exit(1);
+        // }
+
+        float current_progress_percent = angle / 360.0f * 100;
 
         if (current_progress_percent - progress_percent > 1) {
             progress_percent = current_progress_percent;
@@ -517,19 +572,21 @@ args:
         }
         h_black_holes.clear();
         h_black_holes.push_back(BlackHole(500, Vector(0, 0, 0)));
-        h_black_holes.push_back(BlackHole(500, Vector(0, 0, black_hole_distance)));
+        // h_black_holes.push_back(BlackHole(500, Vector(0, 0, black_hole_distance)));
         
         cudaHandleError(cudaMemcpy(d_black_holes, h_black_holes.data(), h_black_holes.size() * sizeof(BlackHole), cudaMemcpyHostToDevice));
         auto other_end = std::chrono::high_resolution_clock::now();
-        double other_duration = std::chrono::duration<double, std::milli>(other_end - other_start).count();
+        float other_duration = std::chrono::duration<float, std::milli>(other_end - other_start).count();
         printf("frame%03d other time: %f\n", (int)(angle*2.), other_duration);
     }
     auto t_end = std::chrono::high_resolution_clock::now();
-    double duration = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+    float duration = std::chrono::duration<float, std::milli>(t_end - t_start).count();
     printf("Time: %f ms\n", duration);
     printf("Estimated fps: %f\n", 160 / (duration / 1000));
 
     printf("Done\n");
+
+    glfwTerminate();
 
     //TODO: free memory
     
