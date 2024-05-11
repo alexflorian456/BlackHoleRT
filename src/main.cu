8b8812538a770b1c9de506a7d23ee7712e77b411
Florian Alex Dan <flalexaris345@gmail.com>
2024-05-11 19:33:49 +0300

    Implemented adaptable ray stepping

--- src/main.cu
+++ src/main.cu
@@ -23,12 +23,11 @@
 #define GRAVITATIONAL_CONSTANT 6.674e-3f // real gravitational constant is 6.674e-11
 #define RAY_TIME_RESOLUTION 1 // time between two ray positions when computing gravitational lensing
 #define ESCAPE_SPHERE_ITERATIONS 10
-#define ESCAPE_SPHERE_RADIUS 2
+#define ESCAPE_SPHERE_RADIUS 5
 
-//TODO: change dynamically based on distance to closest BH
-#define RAY_MAX_ITERATIONS 100 // maximum amount of ray positions computed per pixel
+#define RAY_MAX_ITERATIONS 50 // maximum amount of ray positions computed per pixel
 
-void cudaHandleError(cudaError_t cudaResult){ // TODO?: convert to macro
+void cudaHandleError(cudaError_t cudaResult){ // TODO: (low priority) convert to macro
     if (cudaResult != cudaSuccess) {
         fprintf(stderr, "CUDA handle error: %s: %s\n", cudaGetErrorName(cudaResult), cudaGetErrorString(cudaResult));
         exit(1);
@@ -62,7 +61,7 @@ initOpenGL(int width, int height) {
 }
 
 class 
-Vector{ // TODO: change to float or half? - maybe template
+Vector{
 
     public:
         float x;
@@ -83,7 +82,7 @@ Vector{ // TODO: change to float or half? - maybe template
 
         __device__ __host__ float
         length() const{
-            return std::sqrt(length_2());
+            return std::sqrtf(length_2());
         }
 
         __device__ __host__ Vector
@@ -118,7 +117,7 @@ Vector{ // TODO: change to float or half? - maybe template
         }
 
         __device__ __host__ float
-        azimuth_angle_on_xOz() const{ // TODO: low priority, implement azimuth for any plane
+        azimuth_angle_on_xOz() const{ // TODO: (low priority) implement azimuth for any plane
             Vector projection_on_xOz = Vector(x, 0, z);
             float projection_north_angle = projection_on_xOz.angle(Vector::North());
             return projection_on_xOz.z > 0 ?
@@ -205,7 +204,7 @@ Vector{ // TODO: change to float or half? - maybe template
 };
 
 class
-Color{ // TODO: change to float or half? - maybe template
+Color{
     public:
         unsigned int red;
         unsigned int green;
@@ -262,7 +261,6 @@ Camera{
         float front_plane_distance;
         float back_plane_distance;
 
-        //TODO: init in constructor
         float elevation_angle; // between 0 and 180; 0 <=> straight up, 180 <=> straight down
         float azimuth_angle; // between 0 and 360
 
@@ -351,6 +349,11 @@ extract_texture_color(cudaTextureObject_t texture_object, float i, float j){
     );
 }
 
+__device__ float
+cauchy_bell(float x, float c){
+    return 1.f / ((x * c) * (x * c) + 1.f); 
+}
+
 __global__ void // TODO: recycle variables, use scope operators to conserve register space
 ray(
 Camera camera,
@@ -361,7 +364,7 @@ int grid_index, int block_size, // currently, even if ray is called as a "remain
                                 // in order for the image coordinate arithmetic to be correct
 BlackHole * black_holes, int num_black_holes,
 bool anti_aliasing, bool dynamic_ray_resolution,
-// int frames_processed, // for experimental effects based on frame number
+int frames_processed, // for experimental effects based on frame number
 float * d_ray_direction_vector_map,
 float * d_ray_resolution_multiplier_map
 ){
@@ -378,7 +381,7 @@ float * d_ray_resolution_multiplier_map
 
     // gravitational lensing computation
     Vector old_position = camera.position;
-    Vector old_velocity = ray_direction.normalize(); // TODO?: try normalize
+    Vector old_velocity = ray_direction.normalize();
     Vector new_position = Vector::Zero();
     Vector new_velocity = Vector::Zero();
     constexpr float gravitational_constant = GRAVITATIONAL_CONSTANT;
@@ -389,8 +392,9 @@ float * d_ray_resolution_multiplier_map
 
     bool ray_going_opposite = true;
     float min_impact_parameter = (black_holes[0].position - camera.position).length();
-    float average_distance = 0;
-    float min_distance = min_impact_parameter;
+    float ray_travel_distance;
+    float closest_black_hole_distance = (black_holes[0].position - camera.position).length();
+    float closest_black_hole_mass = black_holes[0].mass;
     for(int black_hole_index = 0; black_hole_index<num_black_holes; black_hole_index++){
         Vector camera_to_black_hole = (black_holes[black_hole_index].position - camera.position);
         Vector ray_to_black_hole_projection = Vector::projection(ray_direction, camera_to_black_hole);
@@ -402,19 +406,16 @@ float * d_ray_resolution_multiplier_map
         if(impact_parameter < min_impact_parameter){
             min_impact_parameter = impact_parameter;
         }
-        
-        average_distance += camera_to_black_hole.length();
-        
-        if(camera_to_black_hole.length() < min_distance){
-            min_distance = camera_to_black_hole.length();
+        if(camera_to_black_hole.length() < closest_black_hole_distance){
+            closest_black_hole_distance = camera_to_black_hole.length();
+            closest_black_hole_mass = black_holes[black_hole_index].mass;
         }
     }
-    average_distance /= num_black_holes;
     
-    // TODO: parameterize the impact parameter multiplier
-    float ray_resolution_multiplier = 1.f / ((min_impact_parameter /* TODO times impact parameter multiplier*/) * (min_impact_parameter /* TODO times impact parameter multiplier*/) + 1.f); // f(x) = 1/((x/25)^2 + 1) bell-like function    
-    if(ray_going_opposite){ // TODO: find better solution
-        d_ray_resolution_multiplier_map[i * output_width + j] = 0; // so that anti-aliasing doesnt blur the skybox opposite to black holes 
+    float distance_mass_cauchy_dropoff_rate = closest_black_hole_distance / (closest_black_hole_mass / 5);
+    float ray_resolution_multiplier = cauchy_bell(min_impact_parameter, (distance_mass_cauchy_dropoff_rate > 1.f) ? 1.f : distance_mass_cauchy_dropoff_rate);    
+    if(ray_going_opposite && distance_mass_cauchy_dropoff_rate > 1.f){ // TODO: find better solution
+        d_ray_resolution_multiplier_map[i * output_width + j] = 1.f / distance_mass_cauchy_dropoff_rate; // so that anti-aliasing doesn't blur the skybox opposite to black holes 
     }
     else{
         d_ray_resolution_multiplier_map[i * output_width + j] = ray_resolution_multiplier;
@@ -433,24 +434,35 @@ float * d_ray_resolution_multiplier_map
         ray_iterations = RAY_MAX_ITERATIONS;
     }
 
-    escape_sphere_radius *= ray_time_resolution;
+    // escape_sphere_radius *= ray_time_resolution;
+    // ray_travel_distance *= ray_time_resolution;
 
-    for(int iter=0; iter<ray_iterations; iter++){ // TODO: replace with ray_iterations for better performance but decreased quality
+    for(int iter=0; iter<ray_iterations; iter++){
         Vector resultant_force = Vector::Zero();
+        float closest_black_hole_to_ray_distance = (old_position - black_holes[0].position).length();
+        float closest_black_hole_to_ray_mass = black_holes[0].mass;
         for(int black_hole_index=0; black_hole_index<num_black_holes; black_hole_index++){ // TO STUDY?: extract to separate kernel
             Vector black_hole_position = black_holes[black_hole_index].position;
             float black_hole_mass = black_holes[black_hole_index].mass;
             float r_squared = (black_hole_position - old_position).length_2();
             Vector r_hat = (black_hole_position - old_position).normalize();
-            resultant_force += r_hat * gravitational_constant * black_hole_mass * 2 / r_squared;
+            resultant_force += r_hat * gravitational_constant * black_hole_mass * 4 / r_squared;
 
+            float r = std::sqrtf(r_squared);
+            if(r < closest_black_hole_to_ray_distance){
+                closest_black_hole_to_ray_distance = r;
+                closest_black_hole_to_ray_mass = black_hole_mass;
+            }
             // spin experiment
             // resultant_force += ((old_position - black_holes[black_hole_index].position).normalize() ^ Vector::North()) / (old_position - black_holes[black_hole_index].position).length_2();
         }
-        average_distance /= 2; // binary search-like ray
-        average_distance = average_distance > 1.f ? average_distance : 1.f;
-        new_velocity = (old_velocity + resultant_force * average_distance).normalize() * old_velocity.length();
-        new_position = old_position + new_velocity * average_distance;
+        // TODO: try making the ray_travel_distance in terms of the magnitude of resultant_force, clipping it to the distance to the closest black hole
+        // ray_travel_distance /= 2; // binary search-like ray
+        ray_travel_distance = (1.f - cauchy_bell(closest_black_hole_to_ray_distance, 1.f / std::powf(closest_black_hole_to_ray_mass * GRAVITATIONAL_CONSTANT, 3))) * closest_black_hole_to_ray_distance;
+        // ray_travel_distance *= ray_time_resolution;
+        ray_travel_distance = ray_travel_distance > 1.f ? ray_travel_distance : 1.f;
+        new_velocity = (old_velocity + resultant_force * ray_travel_distance).normalize() * old_velocity.length();
+        new_position = old_position + new_velocity * ray_travel_distance;
 
         if((new_position - escape_sphere_center).length() > escape_sphere_radius){
             // the ray has escaped the sphere, reset the iteration counter and set
@@ -487,6 +499,15 @@ int block_size, int output_width, int output_height
 ){
     int i = blockIdx.x;
     int j = threadIdx.x + grid_index * block_size;
+
+    if(d_ray_direction_vector_map[i * output_width * 3 + j * 3 + 0] == 0.f &&
+       d_ray_direction_vector_map[i * output_width * 3 + j * 3 + 1] == 0.f &&
+       d_ray_direction_vector_map[i * output_width * 3 + j * 3 + 2] == 0.f){
+        d_output_ray_direction_vector_map[i * output_width * 3 + j * 3 + 0] = 0.f;
+        d_output_ray_direction_vector_map[i * output_width * 3 + j * 3 + 1] = 0.f;
+        d_output_ray_direction_vector_map[i * output_width * 3 + j * 3 + 2] = 0.f;
+        return;    
+    }
 
     Vector sum = Vector::Zero();
     for(int n = 0; n < gaussian_kernel_size; n++){
@@ -536,10 +557,11 @@ int output_width
     // float skybox_height_coordinate = elevation_angle / 180.f;
     // float skybox_width_coordinate  = azimuth_angle   / 360.f;
 
-    Color skybox_color = Color(0, 0, 0, 255);
+    Color skybox_color = Color::Black();
 
-    float epsilon = ray_resolution_multiplier / 7;
-    if(anti_aliasing){
+    // TODO: weigh the perturbed rays based on a 3x3 gaussian kernel
+    float epsilon = (1 - cauchy_bell(ray_resolution_multiplier, 1.f/2.f)) * ray_resolution_multiplier;
+    if(anti_aliasing){ // TODO: (low priority) try perturbing the ray only on the plane with the black hole with the minimum impact parameter
         skybox_color = skybox_color + extract_texture_color(skybox_texture_object, (elevation_angle - epsilon) / 180.f, (azimuth_angle - epsilon) / 360.f);
         skybox_color = skybox_color + extract_texture_color(skybox_texture_object, (elevation_angle - epsilon) / 180.f, (azimuth_angle -    0   ) / 360.f);
         skybox_color = skybox_color + extract_texture_color(skybox_texture_object, (elevation_angle - epsilon) / 180.f, (azimuth_angle + epsilon) / 360.f);
@@ -561,8 +583,7 @@ int output_width
         skybox_color = extract_texture_color(skybox_texture_object, (elevation_angle -    0   ) / 180.f, (azimuth_angle -    0   ) / 360.f);
     }
 
-    // TODO: merge into single function, maybe discard use of Color class
-    set_image_pixel(output_pixels, i, j, output_width, skybox_color); // TODO: shared mem
+    set_image_pixel(output_pixels, i, j, output_width, skybox_color); // TODO: shared mem?
 }
 
 std::vector<std::vector<float>> generate_gaussian_kernel(int size, float sigma) {
@@ -751,7 +772,8 @@ main(int argc, char * argv[]){
 
     // declaring black holes
     h_black_holes.push_back(BlackHole(500, Vector(0, 0, 0)));
-    h_black_holes.push_back(BlackHole(500, Vector(0, 0, 40)));
+    h_black_holes.push_back(BlackHole(500, Vector(0, 0, 500)));
+    h_black_holes.push_back(BlackHole(500, Vector(0, 0, 1000)));
     int num_black_holes = (int)h_black_holes.size();
 
     // allocating device memory
@@ -789,7 +811,7 @@ main(int argc, char * argv[]){
     float angle = 0;
     float progress_percent = -1;
     auto t_start = std::chrono::high_resolution_clock::now();
-    camera.position = Vector(60, 0, 0);
+    camera.position = Vector(100, 0, 0);
 
     // float black_hole_distance = 40;
 
@@ -808,14 +830,13 @@ main(int argc, char * argv[]){
     double mouse_y;
     float delta_mouse_x;
     float delta_mouse_y;
-    while(!glfwWindowShouldClose(main_window)){ // TODO?: call multiple kernels from multiple threads
+    while(!glfwWindowShouldClose(main_window)){ // TODO: pipelining
         // SPIN IN THE CENTER:
         // camera.direction = Vector(std::cos(degrees_to_radians(angle)), 0, std::sin(degrees_to_radians(angle)));
         // ORBIT AROUND CENTER:
         // camera.position = Vector(std::cos(degrees_to_radians(angle)), 0, std::sin(degrees_to_radians(angle))) * camera_distance_from_center;
         // camera.direction = (Vector::Zero() - camera.position).normalize();
         // MOVE INTO CENTER:
-        // TODO: issue noticed when camera is at "perfect" integer coordinates, got CUDA error: an illegal memory access was encountered - NaN values?
         // camera.position = camera.position + camera.direction / 2;
 
         int remaining_width = output_width;
@@ -829,10 +850,10 @@ main(int argc, char * argv[]){
         auto frame_start = std::chrono::high_resolution_clock::now();
         while(remaining_width > 0){
             if(remaining_width >= threads_per_block){
-                ray<<<output_height, threads_per_block>>>(camera, skybox_texture_object, output_width, output_height, grid_index, threads_per_block, d_black_holes, num_black_holes, anti_aliasing, dynamic_ray_resolution, /*frames_processed,*/ d_ray_direction_vector_map, d_ray_resolution_multiplier_map);
+                ray<<<output_height, threads_per_block>>>(camera, skybox_texture_object, output_width, output_height, grid_index, threads_per_block, d_black_holes, num_black_holes, anti_aliasing, dynamic_ray_resolution, frames_processed, d_ray_direction_vector_map, d_ray_resolution_multiplier_map);
             }
             else{ // remainder kernel call
-                ray<<<output_height, remaining_width>>>(camera, skybox_texture_object, output_width, output_height, grid_index, threads_per_block, d_black_holes, num_black_holes, anti_aliasing, dynamic_ray_resolution, /*frames_processed,*/ d_ray_direction_vector_map, d_ray_resolution_multiplier_map);                
+                ray<<<output_height, remaining_width>>>(camera, skybox_texture_object, output_width, output_height, grid_index, threads_per_block, d_black_holes, num_black_holes, anti_aliasing, dynamic_ray_resolution, frames_processed, d_ray_direction_vector_map, d_ray_resolution_multiplier_map);                
             }
             cudaError_t cudaError = cudaGetLastError();
             if (cudaError != cudaSuccess) {
@@ -933,9 +954,6 @@ main(int argc, char * argv[]){
         if(delta_mouse_x != 0 || delta_mouse_y != 0){
             camera.update_direction(-look_speed * delta_mouse_x, look_speed * delta_mouse_y);
         }
-        // if(delta_mouse_x != 0){
-        //     camera.increment_azimuth(-look_speed * delta_mouse_x);
-        // }
 
         glBindTexture(GL_TEXTURE_2D, screen_texture_id);
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, output_width, output_height, GL_RGB, GL_UNSIGNED_BYTE, h_pixels);
@@ -978,7 +996,8 @@ main(int argc, char * argv[]){
         // }
         h_black_holes.clear();
         h_black_holes.push_back(BlackHole(500, Vector(0, 0, 0)));
-        h_black_holes.push_back(BlackHole(500, Vector(0, 0, clip(40.f - angle/64.f, 0, 40))));
+        h_black_holes.push_back(BlackHole(500, Vector(0, 0, clip(500.f - angle/4.f, 0, 1000))));
+        h_black_holes.push_back(BlackHole(500, Vector(0, 0, clip(1000.f - 2*angle/4.f, 0, 1000))));
         
         cudaHandleError(cudaMemcpy(d_black_holes, h_black_holes.data(), h_black_holes.size() * sizeof(BlackHole), cudaMemcpyHostToDevice));
         auto other_end = std::chrono::high_resolution_clock::now();
